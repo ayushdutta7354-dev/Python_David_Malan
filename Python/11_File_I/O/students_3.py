cca7d1b46113531dfe05b3694a69fd86d0f0e333
ayushdutta7354-dev <ayush.dutta7354@gmail.com>
2026-01-28 06:22:52 -0800

    Update students_3.py

--- Python/11_File_I/O/students_3.py
+++ Python/11_File_I/O/students_3.py
@@ -3,10 +3,12 @@ import csv;
 friends_ = [];
 with open("Student_3.csv") as file:
     reader = csv.DictReader(file); #csv.reader reades entire file.
+    # make sure your csv file contains the name of columns, eg here: name, house
     for row in reader:
         friends_.append({"name": row["name"], "house": row["house"], "address":row["address"]});
 
 for friend in sorted(friends_, key=lambda friend: friend["house"]):
     print(f"{friend["name"]} lives in {friend["house"]}");
+
 
 print(friends_);
